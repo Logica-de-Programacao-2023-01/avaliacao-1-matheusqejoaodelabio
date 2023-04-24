6b60a4090cdfc50aae8f1540e9e72dc277db3200
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-04-24 10:39:11 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,7 +1,18 @@
 package q2
 
+func ProblemsSolved(answers [][2]bool) int {
+	count := 0
+	for i := 0; i < len(answers); i++ {
+		soma := 0
+		for j := 0; j < 3; j++ {
+			if answers[i][j] {
+				soma++
+			}
+		}
 
-func ProblemsSolved(answers [][3]bool) int {
-	// Seu código aqui
-	return 0
+		if soma >= 2 {
+			count++
+		}
+	}
+	return count
 }
