d3c6cdc442637482ab3b1e812843103eed7e6335
mathq10 <126472948+mathq10@users.noreply.github.com>
2023-04-24 10:12:51 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -2,7 +2,6 @@ package q4
 
 import (
 	"fmt"
-	
 )
 
 func ClassifyPrices(prices []int) (int, error) {
@@ -11,20 +10,29 @@ func ClassifyPrices(prices []int) (int, error) {
 	}
 	crescente := make([]int, len(prices))
 	copy(crescente, prices)
-	for i:=0;i <= len(prices);i++ {
-		return 1,nil
+	for i := 0; i < len(prices)-1; i++ {
+		if crescente[i] > crescente[i+1] {
+			break
+
+		}
+		if i == len(prices)-2 {
+			return 1, nil
+		}
 	}
-	
+
 	decrescente := make([]int, len(prices))
 	copy(decrescente, prices)
-	for i := len(prices) - 1; i >= 0; i-- {
+	for i := len(prices) - 1; i > 0; i-- {
+		if decrescente[i] < decrescente[i-1] {
+			break
+		}
 		return 2, nil
 	}
 	if crescente == nil && decrescente == nil {
 		return 3, nil
 	}
 	if len(prices) == 1 {
-		return 3,nil
+		return 3, nil
 	}
-	return 0,nil
+	return 0, nil
 }
